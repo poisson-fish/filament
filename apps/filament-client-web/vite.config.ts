23eda82a42dc09d19512e35a99cc7aa83d7ed298
twin <twin@twin.com>
2026-02-11 00:30:00 -0700

    fix: local installation setup for now

--- apps/filament-client-web/vite.config.ts
+++ apps/filament-client-web/vite.config.ts
@@ -4,7 +4,7 @@ import solid from "vite-plugin-solid";
 export default defineConfig({
   plugins: [solid()],
   server: {
-    host: "127.0.0.1",
+    host: "0.0.0.0",
     port: 4173,
     strictPort: true,
     proxy: {
@@ -21,7 +21,7 @@ export default defineConfig({
     },
   },
   preview: {
-    host: "127.0.0.1",
+    host: "0.0.0.0",
     port: 4173,
     strictPort: true,
   },
